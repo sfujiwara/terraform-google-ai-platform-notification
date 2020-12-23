28c01d102f760feeeb4b5e68ecc0befade8c29cb
sfujiwara <shuhei.fujiwara@gmail.com>
2020-12-24 06:13:09 +0900

    add examples

---
+++ examples/main.tf
@@ -0,0 +1,16 @@
+provider "google-beta" {
+  project = var.project_id
+}
+
+module "ai_platform_notification" {
+  source         = "../"
+  project_id     = var.project_id
+  project_number = var.project_number
+}
+
+resource "google_pubsub_subscription" "ai_platform_log" {
+  name    = "ai-platform-notification"
+  project = var.project_id
+  topic   = module.ai_platform_notification.notification_topic.id
+  ack_deadline_seconds = 60
+}
---
+++ examples/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type    = string
+  default = "<your-project-id>"
+}
+
+variable "project_number" {
+  type = string
+  default = "<your-project-number"
+}
