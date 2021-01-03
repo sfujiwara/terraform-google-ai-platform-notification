2f9cd2abca9114d896509cb5627c0fdd3eb4cc36
sfujiwara <shuhei.fujiwara@gmail.com>
2021-01-03 19:23:09 +0900

    add sample script to subtmit ai platform job

---
+++ bin/ai_platform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A sample script to submit a job to Google AI Platform.
+
+JOB_ID="example_$(date '+%Y%m%d%H%M%S')"
+
+gcloud ai-platform jobs submit training "${JOB_ID}" \
+  --region us-central1 \
+  --master-image-uri gcr.io/cloud-builders/gcloud \
+  -- \
+  --help
