bc76e3ca0dcf0dbff76490b8f31f83133eb4f45f
sfujiwara <shuhei.fujiwara@gmail.com>
2021-03-25 05:21:26 +0900

    Remove THREADS environment variable

--- cloudrun/bin/start.sh
+++ cloudrun/bin/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#THREADS=${THREADS:-2}
-
 echo "PORT: ${PORT}"
-#echo "WEB_CONCURRENCY (WORKERS): ${WEB_CONCURRENCY}"
-#echo "THREADS: ${THREADS}"
+echo "WEB_CONCURRENCY (WORKERS): ${WEB_CONCURRENCY}"
 
 uvicorn server.main:app --host 0.0.0.0 --port "${PORT}"
