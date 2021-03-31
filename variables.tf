afe3f9a7e691b510fa21a6d7a1c8affcaa37a118
sfujiwara <shuhei.fujiwara@gmail.com>
2021-04-01 02:49:50 +0900

    test no diff

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "project_id" {
 
 variable "project_number" {
   type        = string
-  description = "Google Cloud Platform project number"
+  description = "Google Cloud Platform project number."
 }
 
 variable "log_topic" {
